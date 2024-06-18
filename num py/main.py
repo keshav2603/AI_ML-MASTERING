09cfb57a12b14a891fef358a7cdedad00b0a2780
keshav2603 <keshavsingla2603@gmail.com>
2024-06-18 14:40:50 +0530

    started num py for ai/ml learning

---
+++ num py/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+l1= [1,2,3,4]
+arr1 = np.array([20,30,40,50])
+arr2 = np.array([20,30,40,50])
+# print(arr1+arr2)
+# print(arr1-arr2)
+# print(arr1*arr2)
+# print(arr1/arr2)
+# print(l1)
+
+# print(np.empty(2))    print a empty array 
+print(np.arange(2,10,2))
+
+arr3 = np.array([20, 30, True])
+# print(arr3)
